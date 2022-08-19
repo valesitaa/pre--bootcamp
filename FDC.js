524ba394daf6c48e7915e5d3d0ea168fdc9d1495
valesitaa <jacyaravalentina.icccp@gmail.com>
2022-08-19 10:42:59 -0400

    diaagrama en t

--- FDC.js
+++ FDC.js
@@ -4,4 +4,29 @@ function muestraListaDeCompras() {
     frozen.push("croqueta de papa");
     console.log(frozen);
 }
-//muestra un arreglo frozen; brocoli, helado y croqueta de papa.
+//muestra un arreglo frozen; brocoli, helado y croqueta de papa//
+
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+//la funcion te muestra el arreglo completo//
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+/*
+muestra un arreglo movielibrary; bambi, beetlejuice, toy story, zoro
+
+    ----Diagrama en T-----
+
+Variable                  Valor 
+frozen;                   brocoli,helado
+frozen;                   brocoli, helado, croqueta de papa
+auntContactInfo;          Paula, Smith, Calle principal 1234, St.Louis, Mo, 12334
+movieLubrary;             Bambi, E.T., Toy Story
+movieLibrary;             Bambi, E.T., Toy Story, Zoro
+movieLibrary;             Bambi, beetlejuice, Toy Story, Zoro
+*/
