4446d06fe7346f033e096e06a46b651a5a06aa24
valesitaa <jacyaravalentina.icccp@gmail.com>
2022-08-19 10:12:43 -0400

    subiendo contenido.

---
+++ FDC.js
@@ -0,0 +1,7 @@
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+//muestra un arreglo frozen; brocoli, helado y croqueta de papa.
---
+++ Henriquez_Jacyara_JSPrediccion.js
@@ -0,0 +1,30 @@
+<head> 
+    <body>
+    function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+//Naci en 2003/    
+console.log("Nací en " + 2003);
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+/*Console.log("Nací en" *EntradaAñoNacimiento)*/
+
+
+function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    
+    /*    console.log("¡Sumando números!");
+        console.log (num1 is 10);
+        console.log(num2 is 20);
+        var sum = num1 + num2;
+        console.log(sum); 30
+    */
+
+    </body> 
+</head>
