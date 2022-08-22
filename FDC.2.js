87c37b082b71222290fbbed1f3d8e6ad6073df15
valesitaa <jacyaravalentina.icccp@gmail.com>
2022-08-22 08:18:08 -0400

    Desafio de codigos.

--- FDC.2.js
+++ FDC.2.js
@@ -88,3 +88,104 @@ num                        15
 num2                       10
 num2                       20
 */
+
+function timesTwoAgain(num) {
+    console.log('num is', num);
+    var y = num*2;
+    return y;
+}
+var result = timesTwoAgain(3) + timesTwoAgain(5);
+console.log('result is', result);
+
+/* la var result nos pide que sumemos los valores de la funcion num 3+5
+la variable y pide multiplicar num*2
+  console.log 5, 3
+  console.log result is, 16 
+  
+    ----Diagrama en T-----
+
+Variable                  Valor 
+result (num)               3+5
+result (num)                8
+ y                         8*2 
+ result                    16
+  */
+
+
+  function sumNums(num1, num2) {  
+    return num1+num2;
+ }
+ console.log(sumNums(2,3))
+ console.log(sumNums(3,5))
+/* la funcion pide sumar los numeros en el parentecis
+console.log 5
+console.log 8*/ 
+
+function printSumNums(num1, num2) {
+    console.log(num1);   
+    return num1+num2;
+ }
+ console.log(printSumNums(2,3))
+ console.log(printSumNums(3,5))
+/*
+console.log 2
+console.log 5
+console.log 3
+console.log 8
+
+*/ 
+
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,5);
+console.log('result is', result);
+/*
+la funcion nos precenta dos numeros que se suman 
+y que luego se suman entre si para un reslutado final
+console.log sum is 5
+console.log sum is 8
+console.log result is 13
+
+----Diagrama en T-----
+
+Variable                  Valor 
+num1                       2,3
+num1                        5 
+num2                       3,5
+num2                        8
+result                    2,3+3,5
+result                       13
+
+*/
+
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+console.log('result is', result);
+/*
+console.log sum is 5
+console.log sum is 3
+console.log sum is 6
+console.log sum is 3
+console.log sum is 5
+console.log sum is 8
+Console.log result is 19
+
+----Diagrama en T-----
+
+Variable                  Valor 
+sum                      num1+num2
+result                     2+3
+result                      3
+result                     2,1
+result                     2,1
+result                     2,3
+result                      19                      
+
+ */
