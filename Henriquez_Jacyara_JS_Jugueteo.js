8c89ede7e2c0ad3b7019d823624858dafacaec96
valesitaa <jacyaravalentina.icccp@gmail.com>
2022-10-19 11:57:13 -0300

    trabajos

--- Henriquez_Jacyara_JS_Jugueteo.js
+++
@@ -1,9 +0,0 @@
-function MuestrameSiElNiñoPuedeSubiraLaMontañaRusa(AlturaNiño) {
-
-    if (AlturaNiño >= 1.52) {
-        console.log("SubeNiño!!");
-    }
-    else {
-        console.log("ParaElProximoAño");
-    }
-}
